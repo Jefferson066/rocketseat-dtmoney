ab4f5218dc5256485358c384c5ad2c0e9eb5b125
Jefferson Santos <jeffersonscr12@gmail.com>
2021-09-30 10:01:18 -0300

    colocando dados da api na table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transaction{
+  id: number,
+  title: string,
+  amount: number,
+  type: string,
+  category: string,
+  createdAt: string
+}
+
 export function TransactionTable() {
 
-  useEffect(() =>{
+  const [transaction, setTransaction] = useState<Transaction[]>([]);
+
+  useEffect(()=> {
     api.get('/transactions')
-      .then(response => console.log(response.data));
+      .then(response => setTransaction(response.data.transactions));
   },[]);
 
   return(
@@ -22,22 +33,36 @@ export function TransactionTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento website</td>
-            <td className="deposit">R$12000,00</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
+          {transaction.map(transaction => (
 
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw"> -R$1200,00</td>
-            <td>Casa</td>
-            <td>20/06/2021</td>
-          </tr>
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{
+                   new Intl.NumberFormat('pt-BR', {
+                     style: 'currency',
+                     currency: 'BRL',
+                  }).format(transaction.amount)
+                }</td>
+                <td>{transaction.category}</td>
+                <td>{
+                   new Intl.DateTimeFormat('pt-BR').format(
+                     new Date (transaction.createdAt))
+                }</td>
+              </tr>
+
+          ))}
 
         </tbody>
       </table>
     </Container>
   );
 }
+
+/*
+<tr>
+            <td>Desenvolvimento website</td>
+            <td className="deposit">R$12000,00</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+*/
